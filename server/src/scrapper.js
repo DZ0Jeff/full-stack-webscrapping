f37a252a69e0f8fd1ee24a19915036701a542844
Jeferson <mx,jeferson.12@gmail.com>
2021-04-27 00:39:12 -0300

    test scrapper extract points

--- server/src/scrapper.js
+++ server/src/scrapper.js
@@ -1,29 +1,36 @@
 const puppeteer = require('puppeteer');
 
 async function scrapperGames(url){
+    const options = { 
+        // headless: false,
+        args: ['--no-sandbox', "--disable-notifications"]
+    }
+
     console.log('Iniciando extração...')
-    const browser = await puppeteer.launch({ 
-        args: ['--no-sandbox']
-    });
+    const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
     console.log(`Abrindo página ${url}`)
     await page.goto(url);
 
     console.log('Página carregada!');
-    console.log('Buscando conteúdo...');
-    const gameLinks = await page.$x('//*[@id="post-body-2794561203943297067"]/div[2]');
-    gameLinks.forEach( async link => {
-        const text = await link.getProperty('textContent');
-        const name = await text.jsonValue(); 
-        console.log(`${name} Extraído!`);
-    })
-    // const text = await gameLinks.getProperty('textContent');
-    // const name = await text.jsonValue();
 
-    // console.log(`${name} Extraído!`);
+    const [container] = await page.$("#post-body-2794561203943297067 > div:nth-child(4) a")
+    console.log(container)
+    // await page.evaluate(() => {
+    //     console.log('Buscando conteúdo...');
+    //     const nodeLinks = document.querySelectorAll("#post-body-2794561203943297067 > div:nth-child(4) a")
 
-    browser.close();
+    //     const raw_links = [...nodeLinks]
+
+    //     const links = raw_links.map(link => ({ name: link.text, url: link.href }))
+
+    //     console.log(links)
+
+    //     return links
+    // })
+
+    await browser.close();
 
     // console.log(gameLinks)
 } 
