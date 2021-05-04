77cd4542ddbab5062074628b8d91e7742928aaad
Jeff <mx.jeferson.12@gmail.com>
2021-05-03 23:29:05 -0300

    add database sqlite

---
+++ server/src/models/database.js
@@ -0,0 +1,71 @@
+const typeorm = require('typeorm')
+const EntitySchema = require('typeorm').EntitySchema
+
+class Games {
+    constructor(id, title, src, torrentLink){
+        this.id = id
+        this.title = title
+        this.src = src
+        this.torrentLink = torrentLink
+    }
+}
+
+const GameSchema = new EntitySchema({
+    name: "Games",
+    target: Games,
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: true
+        },
+        title: {
+            type: "varchar"
+        }, 
+        src: {
+            type: "varchar"
+        }, 
+        torrentLink: {
+            type: "varchar"
+        }
+    }
+})
+
+async function getConnection(){
+    return await typeorm.createConnection({
+        type: 'sqlite',
+        database: './src/data/games.sqlite',
+        logging: false,
+        entities: [ GameSchema ]
+    })
+}
+
+async function getAllGames(){
+    const connection = await getConnection() 
+    const gamesRepo = connection.getRepository(Games)
+    const games = gamesRepo.find()
+    connection.close()
+    return games
+}
+
+async function insertGames(title, src, torrentLink){
+    const connection = await getConnection()
+    
+    // Create
+    const games = new Games()
+    games.title = title
+    games.src = src
+    games.torrentLink
+
+    // Save
+    const gamesRepo = connection.createRepo(Games)
+    const response = gamesRepo.save(games)
+    console.log('Saved', response)
+
+    // return a list
+    const allGames = await gamesRepo.find()
+    connection.close()
+    return allGames
+}
+
+module.exports = { getAllGames, insertGames }
--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 
 const scrapper = require('./scrapper')
 const files = require('./handleFile')
+const db = require('./models/database')
 
 const PORT = process.env.PORT || 3333
 const server = express()
@@ -17,10 +18,11 @@ server.use((request, response, next) => {
 
 server.get('/games', async (request, response) => {
     // get DB
+    const games = await db.getAllGames()
     // get JSON
-    const creators = await files.loadFile()
+    // const games = await files.loadFile()
 
-    return response.json(creators)
+    return response.json(games)
 })
 
 server.post('/games', async (request, response) => {
@@ -28,10 +30,11 @@ server.post('/games', async (request, response) => {
     // todo: Scrappe games
     const gameData = await scrapper.scrapperGames()
     // tado: add to DB
+    const games = await db.insertGames(gameData.title, gameData.src, gameData.torrentLink)
     // todo:add to json
-    files.saveFile(gameData)
+    // files.saveFile(gameData)
 
-    return response.json({ status: "success" })
+    return response.json(games)
 })
 
 server.listen(PORT, () => console.log(`Server Running in port ${PORT}`))
